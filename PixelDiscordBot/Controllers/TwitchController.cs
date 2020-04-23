37e95dfe9eb6be767353053e604a61a82d4db9fc
Ruto <ruto@protonmail.ch>
2020-04-23 18:41:12 -0400

    Initial project

---
+++ PixelDiscordBot/Controllers/TwitchController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PixelDiscordBot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TwitchController : Controller
+    {
+        
+    }
+}
